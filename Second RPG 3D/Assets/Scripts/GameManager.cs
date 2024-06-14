92b342f4e91d7ce6ca02bdaf22430d720def4a3b
Vo_Duyen <long1862004@gmail.com>
2024-06-14 17:49:31 +0700

    ud do hoa va mua trong hem

--- Second RPG 3D/Assets/Scripts/GameManager.cs
+++ Second RPG 3D/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public enum enemyState
 {
@@ -20,10 +21,19 @@ public class GameManager : MonoBehaviour
     public float slimeLookAtSpeed;
 
     public TMP_Text tx;
+
+    [Header("Rain Manager")]
+    public PostProcessVolume postB;
+    public ParticleSystem rainParticle;
+    private ParticleSystem.EmissionModule rainModule;
+    public int rainRateOverTime;
+    public int rainIncrement;
+    public float rainIncrementDelay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rainModule = rainParticle.emission;
     }
 
     // Update is called once per frame
@@ -36,5 +46,56 @@ public class GameManager : MonoBehaviour
         int tam = int.Parse(tx.text);
         ++tam;
         tx.text = tam.ToString();
+    }
+    public void OnOffRain(bool isRain)
+    {
+        StopCoroutine("RainManager");
+        StopCoroutine("PostBManager");
+        StartCoroutine("RainManager", isRain);
+        StartCoroutine("PostBManager", isRain);
+    }
+    IEnumerator RainManager(bool isRain)
+    {
+        switch (isRain)
+        {
+            case true:
+                for (float i = rainModule.rateOverTime.constant; i < rainRateOverTime; i += rainIncrement)
+                {
+                    rainModule.rateOverTime = i;
+                    yield return new WaitForSeconds(rainIncrementDelay);
+                }
+                rainModule.rateOverTime = rainRateOverTime;
+                break;
+            case false:
+                for (float i = rainModule.rateOverTime.constant; i > 0; i -= rainIncrement)
+                {
+                    rainModule.rateOverTime = i;
+                    yield return new WaitForSeconds(rainIncrementDelay);
+                }
+                rainModule.rateOverTime = 0;
+                break;
+        }
+    }
+    IEnumerator PostBManager(bool isRain)
+    {
+        switch (isRain)
+        {
+            case true:
+                for (float i = postB.weight; i < 1; i += Time.deltaTime)
+                {
+                    postB.weight = i;
+                    yield return new WaitForEndOfFrame();
+                }
+                postB.weight = 1;
+                break;
+            case false:
+                for (float i = postB.weight; i > 0; i -= Time.deltaTime)
+                {
+                    postB.weight = i;
+                    yield return new WaitForEndOfFrame();
+                }
+                postB.weight = 0;
+                break;
+        }
     }
 }
