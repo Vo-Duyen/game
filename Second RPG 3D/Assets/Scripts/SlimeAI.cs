1003caf03172a745395ba59b9d1305160117e3db
trung-epicsoft4 <quoctrung@epicsoft4.com>
2024-06-19 17:35:48 +0700

    ..

--- Second RPG 3D/Assets/Scripts/SlimeAI.cs
+++ Second RPG 3D/Assets/Scripts/SlimeAI.cs
@@ -54,7 +54,7 @@ public class SlimeAI : MonoBehaviour
         amin.SetBool("isWalk", isWalk);
         amin.SetBool("isAlert", isAlert);
 
-        sliderHealtBar.transform.forward = Camera.main.transform.forward * -1;
+        sliderHealtBar.transform.forward = Camera.main.transform.forward * -1; 
     }
     IEnumerator timeIsDie()
     {
